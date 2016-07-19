630b35cb2d8e2e54f73a7f5703048e05b843b6b5
Akshay Iyer <akshay.iyer@nyu.edu>
2016-07-19 01:06:48 -0400

    Added Custom Font, wrote the configuration code

--- Spot/Spot/SpotDiscoverViewController.swift
+++ Spot/Spot/SpotDiscoverViewController.swift
@@ -10,5 +10,19 @@ import UIKit
 
 class SpotDiscoverViewController: UIViewController
 {
-    
+    override func viewDidLoad() {
+        for family: String in UIFont.familyNames()
+        {
+            print("\(family)")
+            for names: String in UIFont.fontNamesForFamilyName(family)
+            {
+                print("== \(names)")
+            }
+        }
+        
+        if let font = UIFont(name: "GothamMedium", size: 14) {
+            self.navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName: font, NSForegroundColorAttributeName: UIColor.init(red: 213.0/255.0, green: 227.0/255.0, blue: 227.0/255.0, alpha: 1.0)]
+        }
+
+    }
 }
