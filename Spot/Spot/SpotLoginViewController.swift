a2c31a8514c4b24b77c431e3a6443b44a5a1bb5f
Akshay Iyer <akshay.iyer@nyu.edu>
2016-08-30 16:53:36 -0400

    Added Login Controller Setup, Web View Controller

---
+++ Spot/Spot/SpotLoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SpotLoginViewController.swift
+//  Spot
+//
+//  Created by Akshay Iyer on 8/30/16.
+//  Copyright © 2016 akshaytiyer. All rights reserved.
+//
+
+import UIKit
+
+class SpotLoginViewController: UIViewController {
+    
+    @IBAction func loginButton(sender: AnyObject) {
+        
+    }
+}
